7b0014a67e3a34afddba9cc594b3c735a6108070
Sebastián Cettour <sebas.cettour83@gmail.com>
2026-01-15 00:30:11 -0300

    Iterando sobre las categorías

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,21 +1,22 @@
-
-'use server';
-import {prisma} from "@/src/lib/prisma";
+"use server";
+import { prisma } from "@/src/lib/prisma";
+import CategoryIcon from "../ui/CategoryIcon";
 
 async function getCategories() {
   return await prisma.category.findMany({});
-  
 }
 
 export default async function OrderSidebar() {
-
   const categories = await getCategories();
-  console.log(categories);
   return (
-    <div className="flex flex-col gap-6">
-      <div className="text-2xl font-bold text-gray-800 mb-4 tracking-tight">
-        Menú
-      </div>
-    </div>
+    <aside className="md:w-72 md:h-screen bg-white">
+      <nav className="mt-10">
+        {categories.map((category) => (
+          <CategoryIcon key={category.id} 
+            category={category}
+          />
+        ))}
+      </nav>
+    </aside>
   );
 }
