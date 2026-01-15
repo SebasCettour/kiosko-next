caa8d76fed2945cd555a26253c98f816a56768c8
Sebastián Cettour <sebas.cettour83@gmail.com>
2026-01-14 23:57:58 -0300

    Data Fetching en Next.js

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,4 +1,14 @@
-export default function OrderSidebar() {
+import {prisma} from "@/src/lib/prisma";
+
+async function getCategories() {
+  return await prisma.category.findMany({});
+  
+}
+
+export default async function OrderSidebar() {
+
+  const categories = await getCategories();
+  console.log(categories);
   return (
     <div className="flex flex-col gap-6">
       <div className="text-2xl font-bold text-gray-800 mb-4 tracking-tight">
