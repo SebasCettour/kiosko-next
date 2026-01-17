35649d7e5feabf69d844710b6c024cbda9f1c500
Sebastián Cettour <sebas.cettour83@gmail.com>
2026-01-17 01:20:06 -0300

    Mostrando los articulos de la orden

--- components/order/OrdenSummary.tsx
+++ components/order/OrdenSummary.tsx
@@ -1,6 +1,6 @@
-"use client"; 
+"use client";
 import { useStore } from "@/src/store";
-
+import ProductDetails from "./ProductDetails";
 
 export default function OrdenSummary() {
   const order = useStore((state) => state.order);
@@ -8,10 +8,14 @@ export default function OrdenSummary() {
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-full p-2 mx-auto overflow-x-auto flex-shrink-0">
       <h3 className="text-2xl font-bold mb-5">Resumen del Pedido</h3>
       {order.length === 0 ? (
-        <p className="text-center my-10 break-words">No hay elementos en el pedido</p>
+        <p className="text-center my-10 break-words">
+          No hay elementos en el pedido
+        </p>
       ) : (
         <div className="mt-5">
-          <p></p>
+          {order.map((item) => (
+            <ProductDetails key={item.id} item={item} />
+          ))}
         </div>
       )}
     </aside>
