1c161d381cfff543cbfdeb28b6b5a1d4b4c13fc8
Sebastián Cettour <sebas.cettour83@gmail.com>
2026-01-17 01:53:20 -0300

    Incrementar, decrementar y eliminar productos de la orden

--- src/store.ts
+++ src/store.ts
@@ -6,6 +6,8 @@ import { it } from "node:test";
 interface Store {
   order: OrderItem[];
   addToOrder: (product: Product) => void;
+  increaseQuantity: (id: Product["id"]) => void;
+  decreaseQuantity: (itemId: string) => void;
   removeFromCart: (itemId: string) => void;
 }
 
@@ -13,18 +15,19 @@ export const useStore = create<Store>((set, get) => ({
   order: [],
   addToOrder: (product) => {
     const { categoryId, imageUrl, ...data } = product;
-    let items : OrderItem[] = []
+    let items: OrderItem[] = [];
 
-    if(get().order.find(item => item.id === data.id)){
-        items = get().order.map(item =>
-            item.id === data.id ? {
-                ...item,
-                quantity: item.quantity + 1,
-                subtotal: (item.price + 1) * item.quantity + 1,
-            } : item
-        );
-
-    }else{
+    if (get().order.find((item) => item.id === data.id)) {
+      items = get().order.map((item) =>
+        item.id === data.id
+          ? {
+              ...item,
+              quantity: item.quantity + 1,
+              subtotal: (item.price + 1) * item.quantity + 1,
+            }
+          : item
+      );
+    } else {
       items = [
         ...get().order,
         { ...data, quantity: 1, subtotal: 1 * product.price },
@@ -35,8 +38,37 @@ export const useStore = create<Store>((set, get) => ({
       order: items,
     }));
   },
+  increaseQuantity: (id) => {
+    set((state) => ({
+      order: state.order.map((item) =>
+        item.id === id
+          ? {
+              ...item,
+              quantity: item.quantity + 1,
+              subtotal: (item.price + 1) * item.quantity + 1,
+            }
+          : item
+      ),
+    }));
+  },
+  decreaseQuantity: (itemId) => {
+    set((state) => ({
+      order: state.order
+        .map((item) =>
+          item.id === Number(itemId) && item.quantity > 1
+            ? {
+                ...item,
+                quantity: item.quantity - 1,
+                subtotal: (item.price + 1) * (item.quantity - 1) + 1,
+              }
+            : item
+        )
+        .filter((item) => item.quantity > 0),
+    }));
+  },
   removeFromCart: (itemId) => {
-    console.log("Removing from cart:", itemId);
-    // Implementation for removing from cart goes here
+    set((state) => ({
+      order: state.order.filter((item) => item.id !== Number(itemId)),
+    }));
   },
 }));
