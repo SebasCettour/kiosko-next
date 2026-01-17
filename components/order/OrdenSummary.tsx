2670287624189b962be9c96602124ee3552d43fb
Sebastián Cettour <sebas.cettour83@gmail.com>
2026-01-16 23:54:33 -0300

    Correcciones

--- components/order/OrdenSummary.tsx
+++ components/order/OrdenSummary.tsx
@@ -5,10 +5,10 @@ import { useStore } from "@/src/store";
 export default function OrdenSummary() {
   const order = useStore((state) => state.order);
   return (
-    <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
+    <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-full p-2 mx-auto overflow-x-auto flex-shrink-0">
       <h3 className="text-2xl font-bold mb-5">Resumen del Pedido</h3>
       {order.length === 0 ? (
-        <p className="text-center my-25">No hay elementos en el pedido</p>
+        <p className="text-center my-10 break-words">No hay elementos en el pedido</p>
       ) : (
         <div className="mt-5">
           <p></p>
