f993629abc8b2ee9fa9adbc367033236d6ade75d
Sebastián Cettour <sebas.cettour83@gmail.com>
2026-01-17 02:01:02 -0300

    Calculando total a pagar

--- components/order/OrdenSummary.tsx
+++ components/order/OrdenSummary.tsx
@@ -1,9 +1,11 @@
 "use client";
+import React, { useMemo } from "react";
 import { useStore } from "@/src/store";
 import ProductDetails from "./ProductDetails";
 
 export default function OrdenSummary() {
   const order = useStore((state) => state.order);
+  const total = useMemo(() => order.reduce((total, item) => total + Number(item.price) * item.quantity, 0), [order]);
   return (
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-full p-2 mx-auto overflow-x-auto flex-shrink-0">
       <h3 className="text-2xl font-bold mb-5">Resumen del Pedido</h3>
@@ -16,6 +18,11 @@ export default function OrdenSummary() {
           {order.map((item) => (
             <ProductDetails key={item.id} item={item} />
           ))}
+          <p className="text-2xl mt-20 text-center">Total a pagar: {" "}
+            <span className="font-bold">
+              {total.toFixed(2)}
+            </span>
+          </p>
         </div>
       )}
     </aside>
