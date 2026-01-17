7ca5e3ebb8e3190f3c4ccf39bbdf50050ae8298b
Sebastián Cettour <sebas.cettour83@gmail.com>
2026-01-17 01:43:20 -0300

    Evitar Duplicados

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { OrderItem } from "./types";
 import { Product } from "@prisma/client";
+import { it } from "node:test";
 
 interface Store {
   order: OrderItem[];
@@ -8,15 +9,30 @@ interface Store {
   removeFromCart: (itemId: string) => void;
 }
 
-export const useStore = create<Store>((set) => ({
+export const useStore = create<Store>((set, get) => ({
   order: [],
   addToOrder: (product) => {
     const { categoryId, imageUrl, ...data } = product;
-    set((state) => ({
-      order: [
-        ...state.order,
+    let items : OrderItem[] = []
+
+    if(get().order.find(item => item.id === data.id)){
+        items = get().order.map(item =>
+            item.id === data.id ? {
+                ...item,
+                quantity: item.quantity + 1,
+                subtotal: (item.price + 1) * item.quantity + 1,
+            } : item
+        );
+
+    }else{
+      items = [
+        ...get().order,
         { ...data, quantity: 1, subtotal: 1 * product.price },
-      ],
+      ];
+    }
+
+    set(() => ({
+      order: items,
     }));
   },
   removeFromCart: (itemId) => {
