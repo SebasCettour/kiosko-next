4f53ed80c0c135ff1b785d13a5884cb28848afa0
Sebastián Cettour <sebas.cettour83@gmail.com>
2026-01-24 23:56:40 -0300

    Creando rutas y layout

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,6 +1,7 @@
 "use server";
 import { prisma } from "@/src/lib/prisma";
 import CategoryIcon from "../ui/CategoryIcon";
+import Logo from "../ui/Logo";
 
 async function getCategories() {
   return await prisma.category.findMany({});
@@ -10,9 +11,11 @@ export default async function OrderSidebar() {
   const categories = await getCategories();
   return (
     <aside className="md:w-72 md:h-screen bg-white rounded-xl shadow-lg p-0 flex flex-col">
+      <Logo />
       <h2 className="text-xl font-extrabold text-amber-600 mb-6 text-center tracking-wide mt-0 pt-6">
         Categorías
       </h2>
+
       <nav className="flex-1 overflow-y-auto flex flex-col gap-2">
         {categories.map((category, idx) => (
           <div key={category.id}>
