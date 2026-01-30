326a84faaa3964e984039185d5f375a79462e15c
Sebastián Cettour <sebas.cettour83@gmail.com>
2026-01-29 23:49:04 -0300

    Obtener las Ordenes Pendientes

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,6 +1,23 @@
 import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
 
-export default function page() {
+async function getPendingOrders() {
+  const orders = await prisma.order.findMany({
+    where: {
+      status: false,
+    },
+    include: {
+      orderProducts: {
+        include: {
+          product: true,
+        },
+      },
+    },
+  });
+  return orders;
+}
+export default async function OrdersPage() {
+  const orders = await getPendingOrders();
   return (
     <>
       <Heading>Administrar ordenes</Heading>
