7f3679fac9e34c53bc53be9c4d503d9e48e28664
Sebastián Cettour <sebas.cettour83@gmail.com>
2026-02-03 23:53:14 -0300

    Creando paginador

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -2,20 +2,29 @@ import ProductTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
 
-async function getProducts() {
+async function getProducts(page: number, pageSize: number) {
+  const skip = (page - 1) * pageSize;
   const products = await prisma.product.findMany({
+    take: pageSize,
+    skip: skip,
     include: {
       category: true,
     },
   });
   return products;
 }
+  
+export type ProductWithCategory = Awaited<
+  ReturnType<typeof getProducts>
+>[number];
 
-export type ProductWithCategory = Awaited<ReturnType<typeof getProducts>>[number];  
+export default async function ProductPage(searchParams: {
+  searchParams: { page: string };
+}) {
+  const page = +searchParams.searchParams.page || 1;
+  const pageSize = 10;
 
-
-export default async function ProductPage() {
-  const products = await getProducts();
+  const products = await getProducts(page, pageSize);
   return (
     <>
       <Heading>Administrar Productos</Heading>
