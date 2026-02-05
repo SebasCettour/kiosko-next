4ecea477822bea2c7ecd20632f7f6c9cdb74a194
Sebastián Cettour <sebas.cettour83@gmail.com>
2026-02-05 13:36:06 -0300

    Validando la creación de Productos

--- components/products/AddProductForm.tsx
+++ components/products/AddProductForm.tsx
@@ -1,8 +1,27 @@
 "use client";
-import ProductForm from "./ProductForm";
 
-export default async function AddProductForm({ children }: { children: React.ReactNode }) {
-  const handleSubmit = async (formData: FormData) => {};
+import { ProductSchema } from "@/src/schema";
+import { toast } from "react-toastify";
+
+export default async function AddProductForm({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const handleSubmit = async (formData: FormData) => {
+    const data = {
+      name: formData.get("name"),
+      price: formData.get("price"),
+      categoryId: formData.get("categoryId"),
+    };
+    const result = ProductSchema.safeParse(data);
+    if (!result.success) {
+      result.error.issues.forEach((issue) => {
+        toast.error(issue.message);
+      });
+      return;
+    }
+  };
 
   return (
     <div
