0a9f555eba48ebd7c5c237aa23484ecc1b875d9c
Sebastián Cettour <sebas.cettour83@gmail.com>
2026-02-09 15:27:28 -0300

    Revalidación manual

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -3,6 +3,7 @@ import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
 
 import { OrderWithProducts } from "@/src/types";
+import { revalidatePath } from "next/cache";
 
 async function getPendingOrders(): Promise<OrderWithProducts[]> {
   const orders = await prisma.order.findMany({
@@ -17,13 +18,28 @@ async function getPendingOrders(): Promise<OrderWithProducts[]> {
       },
     },
   });
+
   return orders as OrderWithProducts[];
+}
+
+async function refreshOrders() {
+  "use server";
+  revalidatePath("/admin/orders");
 }
 export default async function OrdersPage() {
   const orders = await getPendingOrders();
   return (
     <>
       <Heading>Administrar ordenes</Heading>
+      <form
+        action={refreshOrders}
+      >
+        <input
+          type="submit"
+          value="Actualizar ordenes"
+          className="bg-amber-400 hover:bg-amber-500 text-white font-bold py-2 px-4 rounded transition-colors duration-300 cursor-pointer mb-4"
+        ></input>
+      </form>
 
       {orders.length ? (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5 h-full">
