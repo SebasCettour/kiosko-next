36d2ed370be979211b1777c760c2e4a024ced05e
Sebastián Cettour <sebas.cettour83@gmail.com>
2026-02-10 14:29:31 -0300

    Correcciones

--- app/admin/products/new/page.tsx
+++ app/admin/products/new/page.tsx
@@ -2,13 +2,14 @@ import AddProductForm from "@/components/products/AddProductForm";
 import ProductForm from "@/components/products/ProductForm";
 import Heading from "@/components/ui/Heading";
 import React from "react";
+import { categories } from "@/prisma/data/categories";
 
 export default function NewProductPage() {
   return (
     <>
       <Heading>Nuevo Producto</Heading>
       <AddProductForm>
-        <ProductForm />
+        <ProductForm categories={categories.map((cat, idx) => ({ id: idx + 1, name: cat.name }))} />
       </AddProductForm>
     </>
   );
